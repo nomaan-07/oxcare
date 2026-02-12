17c6dbcadcea25b68ebc5f5ef6c5e6205a64fb2c
nomaan-07 <nomaan07.dev@gmail.com>
2026-02-12 16:02:52 +0330

    feat: add hero section

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -1,3 +1,34 @@
+import heroImage from "../../../assets/hero.png";
+import { Button } from "../../common/Button";
+import { ButtonsWrapper } from "../../common/Buttons";
+import { Container } from "../../common/Container";
+import styles from "./Hero.module.css";
+
 export function Hero() {
-  return <div>hero</div>;
+  return (
+    <div className={styles.hero}>
+      <Container>
+        <div className={styles.wrapper}>
+          <div>
+            <h2 className={styles.title}>
+              We will help you to improve your mental health
+            </h2>
+            <p className={styles.description}>
+              Evidence-based support designed to strengthen your resilience and
+              improve your daily well-being.
+            </p>
+            <ButtonsWrapper>
+              <Button variant="primary">Get Appointment</Button>
+              <Button variant="secondary" hasBorder>
+                Find Specialist
+              </Button>
+            </ButtonsWrapper>
+          </div>
+          <div>
+            <img className={styles.image} src={heroImage} alt="Hero" />
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 }
